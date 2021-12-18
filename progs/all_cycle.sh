02167ac6ab4b843ab87b4e26c747c1a91bb5e152
dkmur <dkmur74@gmail.com>
2021-12-18 17:44:25 +0000

    initial commit

---
+++ progs/all_cycle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+Home=$HOME
+source config.ini
+
+port=1
+while (( $port < 49 )); do
+echo ""
+echo "Disabling port $port"
+snmpset -v $version -c $community -u $username $switch_ip:$switch_port 1.3.6.1.2.1.105.1.1.1.3.1.$port i 2
+sleep 5s
+echo ""
+echo "Enabling port $port"
+snmpset -v $version -c $community -u $username $switch_ip:$switch_port 1.3.6.1.2.1.105.1.1.1.3.1.$port i 1
+port=$((port+1))
+done
