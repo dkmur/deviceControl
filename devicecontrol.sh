810180ce81e9caf422b6703ee60264131b1438a2
dkmur <dkmur74@gmail.com>
2021-12-21 21:12:14 +0000

    add nonGruber

--- devicecontrol.sh
+++ devicecontrol.sh
@@ -11,89 +11,104 @@ echo "snmpset -v $version -c $community -u $username $ip:$port 1.3.6.1.2.1.105.1
 if [[ -z ${1+x} || -z ${2+x} || -z ${3+x} ]]
 then
   # ask the questions
-  echo "No inputs provided, time to ask questions"
+  clear
+  echo ""
+  echo ""
+  echo "Enter name of switch/relay to be used:"
+  read device
+  echo ""
+  echo "Action: stop, start or (power)cycle"
+  read activity
+  echo ""
+  echo "Enter port number or all"
+  read action
+  echo ""
 else
-  # no questions asked
-
-  check=$(grep $1 config.ini | wc -l)
-  if (( $check != 1 ))
-  then
-    echo "switch/relay not found in config.ini, please check seetings"
-    exit
-  fi
-
-  # we could check if we exceed max port/relay number of the device here and exit otherwise
-
-  # get variables for device from config.ini
-  type=$(grep -A1 $1 config.ini | tail -1 | awk '{ print $3 }')
-  ip=$(grep -A2 $1 config.ini | tail -1 | awk '{ print $3 }')
-  port=$(grep -A3 $1 config.ini | tail -1 | awk '{ print $3 }')
-  relays=$(grep -A4 $1 config.ini | tail -1 | awk '{ print $3 }')
-  sleep=$(grep -A5 $1 config.ini | tail -1 | awk '{ print $3 }')
+  device=$1
+  activity=$2
   action=$3
-  if [ $type == poe ]
+fi
+
+check=$(grep $device config.ini | wc -l)
+if (( $check == 0 ))
+then
+  echo "switch/relay not found in config.ini, please check settings"
+  exit
+fi
+
+# we could check if we exceed max port/relay number of the device here and exit otherwise
+
+# get variables
+type=$(grep -A1 $device config.ini | tail -1 | awk '{ print $3 }')
+ip=$(grep -A2 $device config.ini | tail -1 | awk '{ print $3 }')
+port=$(grep -A3 $device config.ini | tail -1 | awk '{ print $3 }')
+relays=$(grep -A4 $device config.ini | tail -1 | awk '{ print $3 }')
+sleep=$(grep -A5 $device config.ini | tail -1 | awk '{ print $3 }')
+if [ $type == poe ]
+then
+  username=$(grep -A6 $device config.ini | tail -1 | awk '{ print $3 }')
+  version=$(grep -A7 $device config.ini | tail -1 | awk '{ print $3 }')
+  community=$(grep -A8 $device config.ini | tail -1 | awk '{ print $3 }')
+fi
+
+# stop poe port(s)
+if [[ $type == poe && $activity == stop ]]
+then
+  if [ $action != all ]
   then
-    username=$(grep -A6 $1 config.ini | tail -1 | awk '{ print $3 }')
-    version=$(grep -A7 $1 config.ini | tail -1 | awk '{ print $3 }')
-    community=$(grep -A8 $1 config.ini | tail -1 | awk '{ print $3 }')
+    stoppoe
   fi
-  # stop poe port(s)
-  if [[ $type == poe && $2 == stop ]]
+  if [ $action == all ]
   then
-    if [ $action != all ]
-    then
-      stoppoe
-    fi
-    if [ $action == all ]
-    then
-      action=1
-      while (( $action <= $relays )); do
-      echo ""
-      echo "Disabling port $action"
-      stoppoe
-      sleep 3s
-      action=$((action+1))
-      done
-    fi
+    action=1
+    while (( $action <= $relays )); do
+    echo ""
+    echo "Disabling port $action"
+    stoppoe
+    sleep 3s
+    action=$((action+1))
+    done
   fi
-  # start poe port
-  if [[ $type == poe && $2 == start ]]
+fi
+# start poe port
+if [[ $type == poe && $activity == start ]]
+then
+  if [ $action != all ]
   then
-    if [ $action != all ]
-    then
-      startpoe
-    fi
-    if [ $action == all ]
-    then
-      action=1
-      while (( $action <= $relays )); do
-      echo ""
-      echo "Eanabling port $action"
-      startpoe
-      sleep $sleep
-      action=$((action+1))
-      done
-    fi
+    startpoe
   fi
-  # cycle poe port
-  if [[ $type == poe && $2 == cycle ]]
+  if [ $action == all ]
   then
-    if [ $action != all ]
-    then
-      startpoe
-    fi
-    if [ $action == all ]
-    then
-      action=1
-      while (( $action <= $relays )); do
-      echo ""
-      echo "Power cycling  port $action"
-      stoppoe
-      sleep 5s
-      startpoe
-      sleep $sleep
-      action=$((action+1))
-      done
-    fi
+    action=1
+    while (( $action <= $relays )); do
+    echo ""
+    echo "Eanabling port $action"
+    startpoe
+    sleep $sleep
+    action=$((action+1))
+    done
+  fi
+fi
+# cycle poe port
+if [[ $type == poe && $activity == cycle ]]
+then
+  if [ $action != all ]
+  then
+    stoppoe
+    sleep 5s
+    startpoe
+  fi
+  if [ $action == all ]
+  then
+    action=1
+    while (( $action <= $relays )); do
+    echo ""
+    echo "Power cycling port $action"
+    stoppoe
+    sleep 5s
+    startpoe
+    sleep $sleep
+    action=$((action+1))
+    done
   fi
 fi
