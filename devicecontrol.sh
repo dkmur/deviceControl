899dd0bef8535033b1ffc10ed428c9f7e57a3f51
dkmur <dkmur74@gmail.com>
2022-01-24 20:40:55 +0100

    remove exit

--- devicecontrol.sh
+++ devicecontrol.sh
@@ -2,7 +2,6 @@
 
 folder=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#source $folder/config.ini
 pathStats=$(grep 'pathStats' $folder/config.ini | awk '{ print $3 }')
 source $pathStats/config.ini
 
@@ -36,13 +35,13 @@ curl --silent --output /dev/null --show-error --fail -u $MADmin_user:$MADmin_pas
 }
 
 logcatDevice(){
-curl --silent  --show-error --fail -O -J -L -u $MADmin_user:$MADmin_pass "$MADmin_url/download_logcat?origin=$origin"  || echo "Failed download logcat" && exit 1
+# curl --silent  --show-error --fail -O -J -L -u $MADmin_user:$MADmin_pass "$MADmin_url/download_logcat?origin=$origin"  || echo "Failed download logcat" && exit 1
+curl --silent  --show-error --fail -O -J -L -u $MADmin_user:$MADmin_pass "$MADmin_url/download_logcat?origin=$origin"
 }
 
 clearGame(){
 curl --silent --output /dev/null --show-error --fail -u $MADmin_user:$MADmin_pass "$MADmin_url/clear_game_data?origin=$origin"  || echo "Failed to clear pogo game data" && exit 1
 }
-
 
 # checks
 if [[ -z ${1+x} || -z ${2+x} ]]
@@ -59,19 +58,24 @@ fi
 # get variables
 origin=$1
 action=$2
-deviceid=$(query "$MAD_DB" "select device_id from settings_device where name = '$origin'") || echo "Cannot query MADdb for device_id" && exit 1
-instance_name=$(query "$MAD_DB" "select b.name from settings_device a, madmin_instance b where a.name = '$origin' and a.instance_id = b.instance_id") || echo "Cannot query MADdb for instance_name" && exit 1
+echo "wtf2"
+deviceid=$(query "$MAD_DB" "select device_id from settings_device where name = '$origin'") || echo "Cannot query MADdb for device_id"
+echo "wtf3"
+instance_name=$(query "$MAD_DB" "select b.name from settings_device a, madmin_instance b where a.name = '$origin' and a.instance_id = b.instance_id") || echo "Cannot query MADdb for instance_name"
+echo "wtf"
 MADmin_url=$(grep -A1 "^MAD_instance_name.*$instance_name" $pathStats/config.ini | tail -1 | awk 'BEGIN { FS = "=" } ; { print $2 }')
 MADmin_user=$(grep -A2 "^MAD_instance_name.*$instance_name" $pathStats/config.ini | tail -1 | awk 'BEGIN { FS = "=" } ; { print $2 }')
 MADmin_pass=$(grep -A3 "^MAD_instance_name.*$instance_name" $pathStats/config.ini | tail -1 | awk 'BEGIN { FS = "=" } ; { print $2 }')
 
-# echo $origin
-# echo $action
-# echo $deviceid
-# echo $instance_name
-# echo $MADmin_url
-# echo $MADmin_user
-# echo $MADmin_pass
+echo $origin
+echo $action
+echo $deviceid
+echo $instance_name
+echo $MADmin_url
+echo $MADmin_user
+echo $MADmin_pass
+
+echo "test"
 
 if [ $action == "pauseDevice" ]
 then
