2022e6415417aec5b3629654e5a3f98d539ff02a
dkmur <dkmur74@gmail.com>
2022-04-17 21:59:48 +0200

    fix date

--- autocycle.sh
+++ autocycle.sh
@@ -26,12 +26,12 @@ webhook=$(grep webhook_maxPort $folder/config.ini | awk '{ print $3 }')
 # just the troublemakers
 troublemakers=$(query "$MAD_DB" "select count(a.device_id) from trs_status a where a.idle = 0 and a.lastProtoDateTime < now() - interval '$noProtoMinutes' minute and (a.lastPogoRestart < now() - interval '$noRestartMinutes' minute or a.lastPogoRestart is NULL) and (a.lastPogoReboot < now() - interval '$noRebootMinutes' minute or a.lastPogoReboot is NULL)")
 #troublemakers=10
-echo "`date +%Y%m%d %H:%M:%S` Troublemakers found: $troublemakers" >> $folder/autocycle.log
+echo "`date '+%Y%m%d %H:%M:%S'` Troublemakers found: $troublemakers" >> $folder/autocycle.log
 
 # check on max to cycle
 if [ $troublemakers -gt $maxPortCycle  ]
 then
-  echo "`date +%Y%m%d %H:%M:%S` Troublemakers exceed maxPortCycle ($maxPortCycle) as set in config.ini, exiting script" >> $folder/autocycle.log
+  echo "`date '+%Y%m%d %H:%M:%S'` Troublemakers exceed maxPortCycle ($maxPortCycle) as set in config.ini, exiting script" >> $folder/autocycle.log
   $pathStats/default_files/discord.sh --username "Autocycle failure" --color "16711680" --avatar "https://i.imgur.com/Y8jxfb9.png" --webhook-url "$webhook" --description "deviceControl autocycle script exit, too many devices require autocycling"
   exit 1
 fi
@@ -42,7 +42,7 @@ origin=$(echo $line | awk '{print $1}')
 relay_name=$(echo $line | awk '{print $2}')
 relay_port=$(echo $line | awk '{print $3}')
 
-echo "`date +%Y%m%d %H:%M:%S` Cycling $origin on relay: $relay_name, port: $relay_port " >> $folder/autocycle.log
+echo "`date '+%Y%m%d %H:%M:%S'` Cycling $origin on relay: $relay_name, port: $relay_port " >> $folder/autocycle.log
 $folder/relay_poe_control.sh $relay_name cycle $relay_port
 sleep 2s
 
